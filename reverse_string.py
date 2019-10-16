fa0992e91bbee625f6767759004b051eed9b1478
ryu <loved.baske14pride10ryu@gmail.com>
2019-10-16 19:55:58 +0900

    initial commit

---
+++ reverse_string.py
@@ -0,0 +1,36 @@
+class Mystack:
+    
+    def __init__(self):
+        self.items = []
+        
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        last = len(self.items) -1
+        return self.items[last]
+    
+    def size(self):
+        return len(self.items) 
+
+
+stack = Mystack()
+
+for str in "Hello Python":
+    stack.push(str)
+
+reverse = ""
+
+while stack.size():
+    reverse += stack.pop()
+
+print(reverse)
+
+
+
